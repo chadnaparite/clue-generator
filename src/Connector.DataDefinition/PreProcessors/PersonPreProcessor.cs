d274c20fa9e8ecc90133da2615e5b1ad6450240c
Richard Naparite <chad.naparite@gmail.com>
2022-02-23 11:37:19 +0800

    Remove some logs.

--- src/Connector.DataDefinition/PreProcessors/PersonPreProcessor.cs
+++ src/Connector.DataDefinition/PreProcessors/PersonPreProcessor.cs
@@ -14,26 +14,18 @@ namespace CluedIn.Connector.DataDefinition.PreProcessors
     {
         public bool Accepts(ExecutionContext context, IEnumerable<IEntityCode> codes)
         {
-            context.Log.LogDebug($"[PreProcessor] PersonPreProcessor.Accepts: {codes}");
-
             return codes.Any(c => c.Origin.Code == "Chad2");
         }
 
         public void Process(ExecutionContext context, IEntityMetadataPart metadata, IDataPart data)
         {
-            context.Log.LogDebug($"[PreProcessor] PersonPreProcessor.Process: {metadata}");
-
             if (metadata.EntityType == EntityType.Person)
                 this.ProcessPerson(context, metadata, data);
         }
 
         private void ProcessPerson(ExecutionContext context, IEntityMetadataPart metadata, IDataPart data)
         {
-            context.Log.LogDebug($"[PreProcessor] PersonPreProcessor.ProcessPerson: {metadata}");
-            context.Log.LogDebug($"[PreProcessor] PersonPreProcessor.ProcessPerson: {metadata.Properties}");
-
-            foreach (var prop in metadata.Properties)
-                context.Log.LogDebug($"[PreProcessor] PersonPreProcessor: {prop}");
+            context.Log.LogDebug($"[PreProcessor] PersonPreProcessor.ProcessPerson: {metadata.DisplayName}");
 
             if (!metadata.Properties.TryGetValue("chad.person.GCI", out var gci) || string.IsNullOrEmpty(gci))
             {
